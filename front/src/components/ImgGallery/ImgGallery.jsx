f091014d3721f69758e9c0cacc60059ad39c96a5
Andrey Barchuk <andreyb@123software.ru>
2018-05-02 17:30:11 +0300

    added get image request and render response

--- front/src/components/ImgGallery/ImgGallery.jsx
+++ front/src/components/ImgGallery/ImgGallery.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './ImgGallery.css';
 
 class ImgGallery extends Component {
     constructor(props) {
         super(props);
-        
+        this.state = {
+            img: []
+        }
         this.imagesReq = this.imagesReq.bind(this);
     }
-    
+
 
     imagesReq = () => {
         axios.get('http://localhost:4200/getimages').then((response) => {
             console.log(response);
+            this.setState({
+                img: response.data
+            });
         });
     }
 
@@ -19,6 +25,19 @@ class ImgGallery extends Component {
         return (
             <div>
                 <button onClick={this.imagesReq}>Get Images</button>
+                <div className='gallery-content'>
+                {/* //TODO: вынести в отдельный космпонент */}
+                    {this.state.img.map((item) => {
+                        return (
+                            <div className='gallery-image'>
+                                <img src={item} alt="lol" />
+                            </div>
+                        )
+                    })}
+                </div>
+
+
+
             </div>
         );
     }
