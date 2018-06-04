e3d2dc50d7713eec2950f4d264676c4034c04b61
Andrey Barchuk <andreyb@123software.ru>
2018-06-04 14:21:10 +0300

    refactor to stateful component

--- front/src/components/Image/Image.jsx
+++ front/src/components/Image/Image.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import FontAwesome from 'react-fontawesome';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 import './Image.css';
 
-const Image = (props) => {
-    return (
-        <figure className="card-templet"><img src={props.item.link} alt="No img" />
-            <figcaption>
-                <h3>Send Mail </h3>
-                <p>We can send this image to your email</p>
-            </figcaption><FontAwesome tag={'i'} name='fas fa-trash-alt'  />
-            
-        </figure>
-    );
-};
+class Image extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.deleteImage = this.deleteImage.bind(this);
+    }
+
+    deleteImage(){
+        this.props.deleteImg();
+    }
+    
+    render() {
+        return (
+            <figure className="card-templet"><img src={this.props.item.link} alt="No img" />
+                <figcaption>
+                    <h3>Send Mail </h3>
+                    <p>We can send this image to your email</p>
+                </figcaption><FontAwesome tag={'i'} name='fas fa-trash-alt' />
+            </figure>
+        );
+    };
+}
+//TODO:
+// const mapDispatchToProps = dispatch => bindActionCreators({
+    
+// }, dispatch);
+
+// const mapStateToProps = state => ({
+    
+// });
 
 export default Image;
