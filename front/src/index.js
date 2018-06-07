734ac25804783f3ded04ac8c911b76287f34e2ba
Andrey Barchuk <mazafakand@gmail.com>
2018-06-07 12:31:16 +0300

    refactored redux store

--- front/src/index.js
+++ front/src/index.js
@@ -2,22 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
+
 
 
 import registerServiceWorker from './registerServiceWorker';
 
 import App from './containers/App';
-import rootReducer from './redusers/rootRedusers';
-
-const middleware = [thunk];
-
-const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(...middleware)),
-  );
+import store from './store/store';
 
 ReactDOM.render(
     <Provider store={store}>
