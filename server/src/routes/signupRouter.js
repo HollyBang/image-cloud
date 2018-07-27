0b89ea421e1e3e2430bd5ba7041cee4cbb913bf9
Andrey Barchuk <mazafakand@gmail.com>
2018-07-27 15:35:21 +0300

    added sign in router on server

--- server/src/routes/signupRouter.js
+++ server/src/routes/signupRouter.js
@@ -4,55 +4,118 @@ const User = require('../models/user');
 
 
 userRouter.post('/signup', (req, res, next) => {
-      const { body } = req;
-      let { password, email } = body;
+  const {
+    body
+  } = req;
+  let {
+    password,
+    email
+  } = body;
 
-      if (!email) {
-        return res.send({
-          success: false,
-          message: 'Error: Email cannot be blank.'
-        });
-      }
-      if (!password) {
-        return res.send({
-          success: false,
-          message: 'Error: Password cannot be blank.'
-        });
-      }
-      email = email.toLowerCase();
-      email = email.trim();
+  if (!email) {
+    return res.send({
+      success: false,
+      message: 'Error: Email cannot be blank.'
+    });
+  }
+  if (!password) {
+    return res.send({
+      success: false,
+      message: 'Error: Password cannot be blank.'
+    });
+  }
+  email = email.toLowerCase();
+  email = email.trim();
 
-      User.find({
-        email: email
-      }, (err, previousUsers) => {
-        if (err) {
-          return res.send({
-            success: false,
-            message: 'Error: Server error'
-          });
-        } else if (previousUsers.length > 0) {
-          return res.send({
-            success: false,
-            message: 'Error: Account already exist.'
-          });
-        }
-        req.session.user = email;
-        const newUser = new User();
-        newUser.email = email;
-        newUser.password = newUser.generateHash(password);
-        newUser.save((err, user) => {
-          if (err) {
-            return res.send({
-              success: false,
-              message: 'Error: Server error'
-            });
-          }
-          return res.send({
-            success: true,
-            message: 'Signed up'
-          });
-        });
+  User.find({
+    email: email
+  }, (err, previousUsers) => {
+    if (err) {
+      return res.send({
+        success: false,
+        message: 'Error: Server error'
       });
-    }); 
+    } else if (previousUsers.length > 0) {
+      return res.send({
+        success: false,
+        message: 'Error: Account already exist.'
+      });
+    }
 
-    module.exports = userRouter;
+    const newUser = new User();
+    newUser.email = email;
+    newUser.password = newUser.generateHash(password);
+    newUser.folderId = newUser.generateIdFolder();
+    newUser.save((err, user) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: Server error'
+        });
+      }
+      return res.send({
+        success: true,
+        message: 'Signed up'
+      });
+    });
+  });
+});
+
+userRouter.post('/signin', (req, res, next) => {
+  const {
+    body
+  } = req;
+  let {
+    password,
+    email
+  } = body;
+
+  if (!email) {
+    return res.send({
+      success: false,
+      message: 'Error: Email cannot be blank.'
+    });
+  }
+  if (!password) {
+    return res.send({
+      success: false,
+      message: 'Error: Password cannot be blank.'
+    });
+  }
+
+  email = email.toLowerCase();
+  email = email.trim();
+
+  User.find({
+    email: email
+  }, (err, users) => {
+    if (err) {
+      console.log('err:', err);
+      return res.send({
+        success: false,
+        message: 'Error: server error'
+      });
+    }
+    if (users.length != 1) {
+      return res.send({
+        success: false,
+        message: 'Error: Invalid'
+      });
+    }
+    const user = users[0];
+    if (!user.validPassword(password)) {
+      return res.send({
+        success: false,
+        message: 'Error: Invalid'
+      });
+    }
+
+
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    req.session.user = email;
+    res.send();
+ 
+  });
+});
+
+module.exports = userRouter;
